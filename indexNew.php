b63866c1bfb02cf723e76344e58318d285f86577
Gabriel Zério <21043296+gabrielzerio@users.noreply.github.com>
2024-09-29 12:19:18 -0300

    corrigindo as bandeiras e adicionando filesize() para leitura dinamica

--- indexNew.php
+++ indexNew.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <title>Trabalho Desenvolvimento WEB</title>
+    <link rel="stylesheet" href="./css/styleNew.css">
 </head>
 
 <body>
     <main>
         <p class="titulo">Bem vindo ao site Web Design em Foco</p>
-        <p class="opcao-linguagem">Escolha o idioma para entrar</p>
+        <div class="container">
         <div class="empresa-info">
             <?php
                 $ponteiro=fopen("./content.txt", "r");
-                $conteudo = fread ($ponteiro, 300);
+                $conteudo = fread ($ponteiro, filesize("./content.txt"));
                 echo $conteudo;
                 fclose ($ponteiro);
             ?>
         </div>
-        <div>
             <a href="./excluir_cookie.php">Escolher outro idioma</a>
         </div>
     </main>
